18f9dd14cce79749f35fe0aa9dc0ca70df51d9d1
imAETHER <36291026+imAETHER@users.noreply.github.com>
2025-12-24 16:17:21 -0300

    1 number typo :(

--- src/main/java/im/aether/doppler/loaders/DopplerForge.java
+++ src/main/java/im/aether/doppler/loaders/DopplerForge.java
@@ -6,12 +6,12 @@ import net.minecraftforge.common.MinecraftForge;
 import im.aether.doppler.addon.PlasmoVC;
 import im.aether.doppler.screen.OptionScreen;
 /^? < 1.20.1 && != 1.19.2 {^/
-import net.minecraftforge.client.ConfigGuiHandler;
-/^?}^/
+/^import net.minecraftforge.client.ConfigGuiHandler;
+^//^?}^/
 /^? <= 1.20.1 && != 1.19.2 {^/
 
-import net.minecraftforge.fml.ModLoadingContext;
-/^?}^/
+/^import net.minecraftforge.fml.ModLoadingContext;
+^//^?}^/
 
 /^? >= 1.19.2 && <= 1.20.1 {^/
 /^import net.minecraftforge.client.ConfigScreenHandler;
@@ -28,7 +28,7 @@ import org.slf4j.LoggerFactory;
 public class DopplerForge {
     private static final Logger LOGGER = LoggerFactory.getLogger("Doppler");
 
-    public DopplerForge(/^? >1.19.2 && <1.20.1 {^/ /^final FMLJavaModLoadingContext context ^//^?}^/) {
+    public DopplerForge(/^? >1.18.2 && <1.20.1 {^/ /^final FMLJavaModLoadingContext context ^//^?}^/) {
         LOGGER.info("Initializing!");
 
         MinecraftForge.EVENT_BUS.addListener((FMLClientSetupEvent event) -> Util.loadConfig());
@@ -37,11 +37,11 @@ public class DopplerForge {
                 () -> new ConfigScreenHandler.ConfigScreenFactory((mc, prevScreen)
                         -> new OptionScreen(prevScreen)));
         ^//^?} else {^/
-        ModLoadingContext.get().registerExtensionPoint(/^? <1.20.1 {^/ConfigGuiHandler.ConfigGuiFactory.class,
-                () -> new ConfigGuiHandler.ConfigGuiFactory((mc, parent) -> new OptionScreen(parent))/^?} else {^/
-                /^ConfigScreenHandler.ConfigScreenFactory.class,
+        ModLoadingContext.get().registerExtensionPoint(/^? <1.20.1 {^//^ConfigGuiHandler.ConfigGuiFactory.class,
+                () -> new ConfigGuiHandler.ConfigGuiFactory((mc, parent) -> new OptionScreen(parent))^//^?} else {^/
+                ConfigScreenHandler.ConfigScreenFactory.class,
                 () -> new ConfigScreenHandler.ConfigScreenFactory((mc, prevScreen)
-                        -> new OptionScreen(prevScreen))^//^?}^/
+                        -> new OptionScreen(prevScreen))/^?}^/
         );
         /^?}^/
         if (ModList.get().isLoaded("plasmovoice")) {
